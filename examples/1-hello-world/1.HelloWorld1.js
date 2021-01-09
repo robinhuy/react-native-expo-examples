fe2d35e661373c23c794373ab09dc28e7d9caad8
Robin Huy <robin@techmaster.vn>
2021-01-09 11:36:19 +0700

    update dependencies

--- examples/1-hello-world/1.HelloWorld1.js
+++ examples/1-hello-world/1.HelloWorld1.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, Text } from "react-native";
 
 export default function HelloWorld1() {
--- examples/1-hello-world/2.HelloWorld2.js
+++ examples/1-hello-world/2.HelloWorld2.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyleSheet, SafeAreaView, Text } from "react-native";
 import Constants from "expo-constants";
 
